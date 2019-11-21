2c8f610da2a3dd3e32f06215b9fb70d5a01ebbde
denis <vdvpie22@gmail.com>
2019-11-21 16:45:02 +0300

    Add change writed data

--- rabbit-src/controllers/apiClient.js
+++ rabbit-src/controllers/apiClient.js
@@ -4,10 +4,10 @@ const apiClient = require('axios');
 exports.save = async (message) => {
   let domain = message.domain
   message = message.data
-  message.request = {
+  /*message.request = {
     ip: '',
     timestamp: new Date().toISOString()
-  }
+  }*/
 
   return await apiClient.post(apiSettings.options.server+'/api/event/'+domain, message, {strictSSL: false});
 }
